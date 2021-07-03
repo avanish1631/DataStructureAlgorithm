52b0f0b85829cc1eabadfb8f44af60ee12eaee53
avanish1631 <86871673+avanish1631@users.noreply.github.com>
2021-07-03 14:27:54 +0530

    RowPattern

---
+++ pattern/RowPattern/src/com/row/RowPattern.java
@@ -0,0 +1,15 @@
+package com.row;
+
+public class RowPattern {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		int n=5;
+		
+		for(i=1; i<=n; i++) {
+			
+			System.out.print("*");
+		}
+	}
+}
